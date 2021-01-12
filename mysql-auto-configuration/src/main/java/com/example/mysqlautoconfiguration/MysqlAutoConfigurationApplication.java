7a35c5345773b1ca70daa434fe0340926af7d489
ThantMyatPhyo <tmp9night@gmail.com>
2021-01-12 15:25:26 +0630

    Initial commit

---
+++ mysql-auto-configuration/src/main/java/com/example/mysqlautoconfiguration/MysqlAutoConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlautoconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlAutoConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlAutoConfigurationApplication.class, args);
+	}
+
+}
---
+++ mysql-auto-configuration/src/test/java/com/example/mysqlautoconfiguration/MysqlAutoConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlautoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlAutoConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
